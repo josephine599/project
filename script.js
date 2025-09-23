31f5f046f916d5d965115062134e5cb576ef384c
josephine anyango <anyangojozey@gmail.com>
2025-09-23 09:37:48 +0300

    Done

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// async function loadMeals() {
-//       try {
-//         const res = await fetch('db.json');
-//         const data = await res.json();
-//         displayMeals(data.meals);
-//       } catch (error) {
-//         console.error("Error loading meals:", error);
-//       }
-//     }
-
-//     function displayMeals(meals) {
-//       const container = document.getElementById('mealsContainer');
-//       container.innerHTML = '';
-
-//       meals.forEach(meal => {
-//         const card = document.createElement('div');
-//         card.className = 'meal-card';
-
-//         card.innerHTML = `
-//           <h2>${meal.name}</h2>
-//           <p><strong>Ingredients:</strong> ${meal.ingredients.join(', ')}</p>
-//           <p><strong>Instructions:</strong> ${meal.instructions}</p>
-//         `;
-
-//         container.appendChild(card);
-//       });
-//     }
-
-//     document.getElementById('searchInput').addEventListener('input', async (e) => {
-//       const searchTerm = e.target.value.toLowerCase();
-//       const res = await fetch('db.json');
-//       const data = await res.json();
-//       const filteredMeals = data.meals.filter(meal =>
-//         meal.name.toLowerCase().includes(searchTerm)
-//       );
-//       displayMeals(filteredMeals);
-//     });
-
-//     loadMeals();
-
 let allMeals = [];
 let darkMode = false;
 
@@ -66,6 +26,7 @@ function displayMeals(meals) {
     const card = document.createElement('div');
     card.className = 'meal-card';
     card.innerHTML = `
+    <img src="${meal.image}" alt="${meal.name}" class="meal-image">
       <h2>${meal.name}</h2>
       <p><strong>Ingredients:</strong> ${meal.ingredients.join(', ')}</p>
       <p><strong>Instructions:</strong> ${meal.instructions}</p>
